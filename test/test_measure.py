5502239e68a5c2da749106b00b0313282b24f11c
El-Tomaz <tomazlemosgarcia@gmail.com>
2025-09-12 13:46:31 -0300

    adicionando testes!

--- test/test_measure.py
+++ test/test_measure.py
@@ -11,6 +11,18 @@ def close_connection():
     yield
     lite.close()
 
+
+@pytest.fixture(autouse=True)
+def setup(request):
+
+    if 'nosetup' in request.keywords: return 
+
+    lite.connect(port_name = "COM8")
+    lite.cfg_sweep(start = 600e6, stop = 930e6, points = 201, n_mean = 2)
+    yield
+    lite.close()
+
+@pytest.mark.nosetup
 def test_connect():
     
     result = lite.connect(port_name = "COM8")
@@ -18,9 +30,7 @@ def test_connect():
 
 
 
-def test_measure_raw():
-    lite.connect(port_name = "COM8")
-    lite.cfg_sweep(start = 200e6, stop = 3e9, points = 201, n_mean = 5)
+def test_measure_raw_s11():
 
     lite.measure()
 
@@ -30,23 +40,40 @@ def test_measure_raw():
 
     ax_raw.plot(freq, s11_r_raw, label = "real")
     ax_raw.plot(freq, s11_i_raw, label = "imag")
-    
+
+    plt.title("raw s11")
     plt.legend()
     plt.show()
 
+def test_measure_raw_s11_logmag():
+    
+    lite.measure()
+    freq, s11_logmag_raw = lite.extract_s11_raw_logmag()
+
+    fig, ax = plt.subplots(1)
+
+    ax.plot(freq,s11_logmag_raw)
+
+    plt.title("S11 log mag")
+    plt.show()
+
+
+
 def test_measure_calib():
-    lite.connect(port_name = "COM8")
-    lite.cfg_sweep(start = 200e6, stop = 3e9, points = 201, n_mean = 5)
 
     lite.calibrate_cli()
 
+    input("Put the mut")
 
     lite.measure()
 
     freq, s11_r_raw, s11_i_raw = lite.extract_s11_raw()
     freq, s11_r, s11_i = lite.extract_s11()
     
+    freq1, s11_logmag = lite.extract_s11_logmag()
+
     fig, [ax_raw, ax_calib] = plt.subplots(2)
+    fig2, ax_logmag = plt.subplots(1)
 
     ax_raw.plot(freq, s11_r_raw, label = "real")
     ax_raw.plot(freq, s11_i_raw, label = "imag")
@@ -55,9 +82,19 @@ def test_measure_calib():
     ax_calib.plot(freq, s11_r, label = "real")
     ax_calib.plot(freq, s11_i, label = "imag")
 
+    ax_logmag.plot(freq1, s11_logmag, label = "logmag")
+
     plt.legend()
     plt.show()
 
+
+def test_save_calib():
+    pass
+    
+
+
+def test_measure_load_calib():
+    pass
 
 
     
